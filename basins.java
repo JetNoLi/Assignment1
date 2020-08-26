96f9382b67048bc3d9dc7fd0733bf7145ffedd17
JetNoLi <hendricks.jet@gmail.com>
2020-08-26 23:38:35 +0200

    Changed Implementation to use Strings for easy return for recursiveTask

--- basins.java
+++ basins.java
@@ -6,17 +6,16 @@ public class basins{
     /** Class to find basins in an N x M grid for Terrain Classification */
 
     //instance variables
-    private boolean[][] booleanGrid; 
+    //private String basinGrid; 
     private float[] linearGrid;
-    private int basinCount;
+    //private int basinCount;
     private int rowLength;
     private int colLength;
-
 
     //constructor
     public basins(){
         /**method to read in gridFile data to instance varaibles */
-        this.basinCount = 0;
+        //this.basinCount = 0;
     }
 
     public void readInGrid(Scanner gridFile){
@@ -26,7 +25,6 @@ public class basins{
         this.colLength = Integer.parseInt(size[1]);
 
         this.linearGrid = new float[rowLength*colLength];
-        this.booleanGrid = new boolean[rowLength-2][colLength-2];
         
         //read in to grid and linearGrid
         for (int i = 0; i< rowLength; i++){
@@ -36,67 +34,61 @@ public class basins{
         }
     }
 
-    public void getBasins(){
+    public boolean basinCheck(int i){
+        //beginning or end of a row( i.e col 0 and last col) 
+        if (i%rowLength == 0 || i%(rowLength-1 + i/rowLength * colLength) == 0){
+            return false;
+        }
 
-        for (int i = 0 ; i < linearGrid.length; i++){
+        //beginning or end of a column (i.e. row 0 and last row)
+        if (i < rowLength || i >= (rowLength-1)*(colLength)){
+            return false;
+        }
 
-            //beginning or end of a row( i.e col 0 and last col) 
-            if (i%rowLength == 0 || i%(rowLength-1 + i/rowLength * colLength) == 0){
-                continue;
-            }
+        //check for basins left and right
+        if (linearGrid[i+1] - linearGrid[i] >= 0.01 && linearGrid[i-1] - linearGrid[i] >= 0.01){
+            //check below
 
-            //beginning or end of a column (i.e. row 0 and last row)
-            if (i < rowLength || i >= (rowLength-1)*(colLength)){
-                continue;
-            }
-
-            //System.out.println(i);
-            //check for basins left and right
-            if (linearGrid[i+1] - linearGrid[i] >= 0.01 && linearGrid[i-1] - linearGrid[i] >= 0.01){
-                //check below
-
-                if (linearGrid[i + rowLength] - linearGrid[i] >= 0.01 && linearGrid[i + rowLength + 1] - linearGrid[i] >= 0.01 && linearGrid[i + rowLength - 1] - linearGrid[i] >= 0.01){
-                    //check above
-                   
-                    if (linearGrid[i - rowLength] - linearGrid[i] >= 0.01 && linearGrid[i - rowLength + 1] - linearGrid[i] >= 0.01 && linearGrid[i - rowLength - 1] - linearGrid[i] >= 0.01){
-                        //is a basin
-                        int[] ind = getBooleanIndices(i);
-                        booleanGrid[ind[0]][ind[1]] = true;
-                        basinCount++;
-                          
-                    } 
-                }
+            if (linearGrid[i + rowLength] - linearGrid[i] >= 0.01 && linearGrid[i + rowLength + 1] - linearGrid[i] >= 0.01 && linearGrid[i + rowLength - 1] - linearGrid[i] >= 0.01){
+                //check above
+               
+                if (linearGrid[i - rowLength] - linearGrid[i] >= 0.01 && linearGrid[i - rowLength + 1] - linearGrid[i] >= 0.01 && linearGrid[i - rowLength - 1] - linearGrid[i] >= 0.01){
+                    //is a basin
+                    return true;           
+                } 
             }
         }
+        return false;
     }
 
-    public void printBasins(){
-        
-        System.out.println(basinCount);
+    public String getBasins(){
+        String basinGrid = "";
+        int basinCount = 0;
 
-        for (int i = 0; i < booleanGrid.length; i++){
-            for (int j = 0; j < booleanGrid[0].length; j++){
-                
-                if (booleanGrid[i][j] == true){
-                    System.out.println((i+1) + " " + (j+1));
-                }
-
-            }   
+        for (int i = 0 ; i < linearGrid.length; i++){
+           if (this.basinCheck(i)){
+            int[] ind = getBasinIndices(i);
+            basinGrid += ind[0] + " " + ind[1] + "\n";
+            basinCount++;
+              
+           }
         }
+
+        return basinCount + "\n" + basinGrid;
     }
 
     public int getLinearizedIndex(int row, int col){
         return (row*rowLength + col); 
     }
 
-    public int[] getBooleanIndices(int index){
+    public int[] getBasinIndices(int index){
         float r = ((float)index)/((float)rowLength);
         float c = r%1;
 
         float row = r - c;
         float col = c * rowLength;
 
-        int[] indices = {(int) row - 1,(int) col - 1};
+        int[] indices = {(int) row ,(int) col };
         return indices;
     }
 
@@ -113,9 +105,7 @@ public class basins{
             System.out.println("Error File not Found");
         }
 
-        basin.getBasins();
-        basin.printBasins();
+        System.out.println(basin.getBasins());
     }
-
 
 }
